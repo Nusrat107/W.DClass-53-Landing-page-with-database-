8211136298b2717b76fe6285aa9a960d6063955e
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-04-27 19:53:15 +0600

    Class-53

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$connection = new mysqli ('localhost', 'root', '', 'landingpage_withdatabase');
+
+// if($connection == true){
+// echo "Connection Successfully!";
+// }
+// else{
+//     echo "Connection Failed!";
+// }
+// ?>
